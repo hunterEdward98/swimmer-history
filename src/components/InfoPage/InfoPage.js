3869c45360d8f508a684acda3a0bdc8f0bfa6868
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-02 18:56:12 -0500

    Clear inputs.

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -8,36 +8,37 @@ import {connect} from 'react-redux';
 
 class InfoPage extends React.Component {
 
-        state = {
-          // ourObj: [],
-          description: '',
-          image_url: ''
-        }
-        
-        componentDidMount(){
-          this.fillShelf();
-        }
-  
-        fillShelf = () => {
+  state = {
+    // ourObj: [],
+    description: '',
+    image_url: ''
+  }
+
+  componentDidMount() {
+    this.fillShelf();
+  }
+
+  fillShelf = () => {
     // Axios.get('/api/shelf').then((response) => {
     //     console.log('this is what we get', response.data)
     //     this.setState({
     //       ourObj: response.data
     //     })
 
-          this.props.dispatch({
-            type: 'FETCH_BOOKS',
-          });
+    this.props.dispatch({
+      type: 'FETCH_BOOKS',
 
-        }
-  
+    });
+
+  }
+
   addInputs = (key, event) => {
     this.setState({ [key]: event.target.value })
   }
 
   submitBook = () => {
 
-    const newBook= { 
+    const newBook = {
       description: this.state.description,
       image_url: this.state.image_url
     }
@@ -49,26 +50,30 @@ class InfoPage extends React.Component {
       type: 'ADD_TO_SHELF',
       payload: newBook
     });
+    this.setState({
+      description: '',
+      image_url: ''
+    })
   }
 
-      render() {
-        return ( 
-           <div>
-             <input placeholder='book description' value={this.state.description} onChange={(event) => this.addInputs('description', event)}/>
-             <input placeholder='image url' value={this.state.image_url} onChange= {(event) => this.addInputs('image_url', event)}/>
-             <button onClick={()=>this.submitBook()}>Submit</button>
-              {this.props.item ?.map(x=><div><div>{x.description}</div><img src={x.image_url}/><button onClick={()=> this.props.dispatch({type:'DELETE_BOOK', payload: x.id})}>Delete</button></div>)}
-           </div>
-        )
-      }
+  render() {
+    return (
+      <div>
+        <input placeholder='book description' value={this.state.description} onChange={(event) => this.addInputs('description', event)} />
+        <input placeholder='image url' value={this.state.image_url} onChange={(event) => this.addInputs('image_url', event)} />
+        <button onClick={() => this.submitBook()}>Submit</button>
+        {this.props.item?.map(x => <div><div>{x.description}</div><img src={x.image_url} /><button onClick={() => this.props.dispatch({ type: 'DELETE_BOOK', payload: x.id })}>Delete</button></div>)}
+      </div>
+    )
+  }
 
-    }
+}
 
 
-    const mapStateToProps = (state) => {
-      return {
-        item: state.item
-      }
-    }
+const mapStateToProps = (state) => {
+  return {
+    item: state.item
+  }
+}
 
-export default connect (mapStateToProps) (InfoPage);
+export default connect(mapStateToProps)(InfoPage);
