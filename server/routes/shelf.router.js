b04fb14aa76296128fc50c7d2a69fc6420e174cf
Luke Johnson <lukejohnson3712@gmail.com>
2020-07-02 18:05:23 -0500

    Update.

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -18,18 +18,30 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
-  
+router.post('/', rejectUnauthenticated, (req, res) => {
+    const desc = req.body.description;
+    const img = req.body.image_url
+    const usr = req.user.id
+    let queryText = 'INSERT INTO item (description, image_url, user_id) VALUES($1,$2,$3)'
+    pool.query(queryText, [desc, img, usr]).then(result => {
+        res.sendStatus(202);
+    }).catch(error => {
+        res.sendStatus(500)
+    })
 });
 
 
 /**
  * Delete an item if it's something the logged in user added
  */
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    let queryText = 'DELETE FROM item WHERE id=$1 AND user_id=$2'
+    pool.query(queryText, [req.params.id, req.user.id]).then(result => {
+        res.sendStatus(203)
+    }).catch(error => {
+        res.sendStatus(500)
+    })
 });
-
 
 /**
  * Update an item if it's something the logged in user added
@@ -54,5 +66,16 @@ router.get('/count', (req, res) => {
 router.get('/:id', (req, res) => {
 
 });
+
+
+
+
+/**
+ * Delete an item if it's something the logged in user added
+ */
+
+
+
+
 
 module.exports = router;
